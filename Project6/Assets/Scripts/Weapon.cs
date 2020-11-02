dcd3478a40f37531ca7192cd8b449af4854c7c53
Jerod Lockhart <60198376+almosthree@users.noreply.github.com>
2020-11-01 23:25:32 -0600

    edited project 6

--- Project6/Assets/Scripts/Weapon.cs
+++ Project6/Assets/Scripts/Weapon.cs
@@ -5,6 +5,18 @@ public class Weapon : MonoBehaviour
 {
     public int damageBonus;
 
+    public Enemy enemyHoldingWeapon;
+
+    private void Awake()
+    {
+        enemyHoldingWeapon = gameObject.GetComponent<Enemy>();
+        EnemyEatsWeapon(enemyHoldingWeapon);
+    }
+
+    protected void EnemyEatsWeapon(Enemy enemy)
+    {
+        Debug.Log("Enemy eats weapon");
+    }
     public void Recharge()
     {
         Debug.Log("Recharging weapons");
